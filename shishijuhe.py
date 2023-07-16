2e5a01706670c9833afa7938c805c16d24d69060
totootao <totootao@qq.com>
2023-07-16 11:32:35 +0800

    rss

--- shishijuhe.py
+++ shishijuhe.py
@@ -10,16 +10,20 @@ domain = 'https://rsshub.rssforever.com/telegram/channel/'
 
 def parse(post):
     item={}
-    item['title'] = post.getElementsByTagName('title')[0].childNodes[0].data
-    item['link'] = post.getElementsByTagName('link')[0].childNodes[0].data
-    item['pubDate'] = post.getElementsByTagName('pubDate')[0].childNodes[0].data
+
     description = post.getElementsByTagName('description')[0].childNodes[0].data
     list = re.findall(r'</a> <a href="(.+?)"', description)
     if not list:
-        item['description'] = description
+        item['title']='delete'
     else:
-        item['link']=list[0]
-        item['description'] = fetch(list[0]).css('div[id=page-content]').get().replace('\n','').replace('\r','')
+        tree = fetch(list[0])
+        if not tree.css('span[id=copyright_logo]'):
+            item['title']='delete'
+        else:
+            item['description'] = tree.css('div[id=page-content]').get().replace('\n','').replace('\r','')
+            item['title'] = post.getElementsByTagName('title')[0].childNodes[0].data
+            item['link'] = post.getElementsByTagName('link')[0].childNodes[0].data
+            item['pubDate'] = post.getElementsByTagName('pubDate')[0].childNodes[0].data
     return item
 
 def ctx(category=''):
@@ -27,7 +31,9 @@ def ctx(category=''):
     tree=parseString(requests.get(url,verify=False).text).documentElement
     posts=tree.getElementsByTagName('item')
     items= list(map(parse,posts))
-
+    for item in items[:]:
+        if item['title'] == 'delete':
+            items.remove(item)
     return {
         'title':'时事聚合-原文',
         'description':'通过全球政治经济评论,预知世界局势发展,把握外汇期货证券行情运行趋势,为您的生活理财服务。',
